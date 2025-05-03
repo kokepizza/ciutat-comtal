83a1db714896398e93326512acde817ffc093a05
Koke Thiele <kokepizzadev@gmail.com>
2025-05-03 08:32:09 +0200

    debounce resize function

--- js/main.js
+++ js/main.js
@@ -4,6 +4,21 @@ console.log("JavaScript carregat correctament.");
 import { initGlobalAnimations } from './animations/global.js';
 initGlobalAnimations();
 
+// Debounce resize function
+let mobile = window.matchMedia("(max-width: 900px)").matches;
+let time = null;
+
+window.addEventListener("resize", function() {
+  clearTimeout(time);
+  time = setTimeout(() => {
+    const isMobile = window.matchMedia("(max-width: 900px)").matches;
+    if (isMobile !== mobile) {
+      window.location.reload();
+    }
+    mobile = isMobile;
+  }, 200); // temps d'espera després del darrer resize
+});
+
 if (window.matchMedia("(max-width: 900px)").matches) {
   import('./animations/mobile.js').then((module) => {
     module.initMobileAnimations();
